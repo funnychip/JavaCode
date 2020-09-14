0c192ae47e0a236af24e7a77e98f2e29cdf2c8bc
funnychip <memory4446@aliyun.com>
2020-09-14 22:34:24 +0800

    ChatRoom

---
+++ ChatRoom/src/main/java/model/Channel.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Channel {
+    //频道ID
+    private int channelId;
+    //频道名
+    private String channelName;
+
+    public int getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(int channelId) {
+        this.channelId = channelId;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    @Override
+    public String toString() {
+        return "Channel{" +
+                "channelId=" + channelId +
+                ", channelName='" + channelName + '\'' +
+                '}';
+    }
+}
---
+++ ChatRoom/src/main/java/model/User.java
@@ -0,0 +1,65 @@
+package model;
+
+
+import java.sql.Timestamp;
+
+// 一个 User 对象就用来表示一条数据库的记录.
+// 对象的属性基本和数据库的表结构一致. (大部分是一致的, 可能有细节上的差异)
+public class User {
+    private int userId;
+    private String name;
+    private String password;
+    private String nickname;
+    private Timestamp lastLogout;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickName() {
+        return nickname;
+    }
+
+    public void setNickName(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Timestamp getLastLogout() {
+        return lastLogout;
+    }
+
+    public void setLastLogout(Timestamp lastLogout) {
+        this.lastLogout = lastLogout;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", lastLogout=" + lastLogout +
+                '}';
+    }
+}
